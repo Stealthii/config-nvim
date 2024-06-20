c0bd9341fcfc14e4cc9317e5ca143bbf862df4aa
Daniel Porter <dpreid@gmail.com>
2024-06-20 09:49:39 +0100

    Move to LazyVim reference for CopilotChat plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
